3cf547de563898ab67abbce9f2e2489919629ca7
Praveen Kumar <praveenscience@gmail.com>
2022-02-20 07:16:56 +0000

    Cleaned up App.

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,10 @@
 import React, { Component } from "react";
 
 class App extends Component {
-  state = {
-    Name: "Dinki Yaduwanshi"
-  };
-  handleChange = e => {
-    this.setState({
-      Name: e.target.value
-    });
-  };
   render() {
     return (
       <div className="App">
-        <h1>
-          {this.state.Name.trim().length > 0 ? this.state.Name : "Everyone"} is
-          awesome! :)
-        </h1>
-        <input
-          type="text"
-          value={this.state.Name}
-          onChange={this.handleChange}
-        />
-        <br />
-        <input
-          type="text"
-          value={this.state.Name}
-          onChange={this.handleChange}
-        />
+        <h1>Dinki's Portfolio</h1>
       </div>
     );
   }
