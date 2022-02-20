233ece8d918077a7d2bcde45ebe77da0f8de3da6
Praveen Kumar <praveenscience@gmail.com>
2022-02-20 07:34:31 +0000

    Dynamicised all the data.

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,16 @@
 import React, { Component } from "react";
 import Header from "./Bootstrap/Header";
+import Data from "../constants/Data.json";
 
 class App extends Component {
   componentDidMount() {
-    document.title = "Dinki's Portfolio";
+    document.title = `${Data.Name.FName} ${Data.Name.LName}'s Portfolio`;
   }
   render() {
     return (
       <div className="App">
         <Header dark={true} className="justify-content-center">
-          Dinki's Portfolio
+          {`${Data.Name.FName} ${Data.Name.LName}'s Portfolio`}
         </Header>
         <div className="container">
           <div className="row">
@@ -17,22 +18,19 @@ class App extends Component {
               <figure className="text-center">
                 <img
                   src="https://i.imgur.com/DQFI0Slh.png"
-                  alt="Dinki"
+                  alt={`${Data.Name.FName} ${Data.Name.LName}`}
                   className="img-thumbnail rounded-circle w-75 my-3"
                 />
                 <figcaption>
-                  <h2>Dinki Yaduwanshi</h2>
-                  <h3>Front End Developer</h3>
+                  <h2>{`${Data.Name.FName} ${Data.Name.LName}`}</h2>
+                  <h3>{Data.Title}</h3>
                 </figcaption>
               </figure>
             </div>
             <div className="col-12 col-md-6">
               <div className="my-3">
-                <h2>Welcome to Dinki's Portfolio</h2>
-                <p className="lead">
-                  Thanks for coming and watching my portfolio! Your presence is
-                  great!
-                </p>
+                <h2>Welcome to {Data.Name.FName}'s Portfolio</h2>
+                <p className="lead">{Data.Desc}</p>
               </div>
             </div>
           </div>
